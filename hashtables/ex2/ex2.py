0de605ac290bd496cc1d7b6cf76b25b1bf08df7a
Elijah McKay <elijahmckay10@gmail.com>
2020-03-13 12:42:46 -0500

    finished hashtable code challenges

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -1,9 +1,7 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
 from hashtables import (HashTable,
                         hash_table_insert,
-                        hash_table_remove,
-                        hash_table_retrieve,
-                        hash_table_resize)
+                        hash_table_retrieve)
 
 
 class Ticket:
@@ -13,11 +11,38 @@ class Ticket:
 
 
 def reconstruct_trip(tickets, length):
-    hashtable = HashTable(length)
-    route = [None] * length
+    ht = HashTable(length)
+    locations = [None] * length  
 
     """
     YOUR CODE HERE
-    """
+    tickets = [
+        Ticket{ source: "PIT", destination: "ORD" },
+        Ticket{ source: "XNA", destination: "CID" },
+        Ticket{ source: "SFO", destination: "BHM" },
+        Ticket{ source: "FLG", destination: "XNA" },
+        Ticket{ source: "NONE", destination: "LAX" },
+        Ticket{ source: "LAX", destination: "SFO" },
+        Ticket{ source: "CID", destination: "SLC" },
+        Ticket{ source: "ORD", destination: "NONE" },
+        Ticket{ source: "SLC", destination: "PIT" },
+        Ticket{ source: "BHM", destination: "FLG" }
+    ]
 
-    pass
+    """
+    for ticket in tickets:
+        # creating map with starting location as key and destination as value
+        hash_table_insert(ht, ticket.source, ticket.destination)
+
+    print(ht)
+
+    for i in range(length):
+        if locations[i - 1] is not None:
+            locations[i] = hash_table_retrieve(ht, locations[i - 1])
+            print(locations[i - 1])
+        else:
+            locations[i] = hash_table_retrieve(ht, "NONE")
+
+    # slicing off the NONE
+    # print(locations)
+    return locations[:-1]
